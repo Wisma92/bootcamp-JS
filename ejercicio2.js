b8c35dc93ce5f4148a86825b6714a4fc00e2dcdd
Rafael Mendoza <112434814+Wisma92@users.noreply.github.com>
2023-06-13 20:02:28 -0500

    Actividad-2

---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+const lista = [
+    'Rafael Mendoza',
+    30,
+    true,
+    new Date(1922,9,24),
+    libro = {
+        libro: 'Los Juegos del hambre',
+        autor: 'Suzanne Collins',
+        fecha: new Date(2008,2,15),
+        url: 'https://www.casadellibro.com.co/libros-los-juegos-del-hambre/755'
+    }
+]
