bb41c898316c55182938a17fb246d7f63f2431c7
Rafael Mendoza <112434814+Wisma92@users.noreply.github.com>
2023-06-21 18:39:42 -0500

    Modulo 4

---
+++ ejercicio_4.js
@@ -0,0 +1,22 @@
+// ejercico 4
+
+let nombre = "Wisma";
+let apellido = "Mendoza";
+let estudiante = nombre.concat(" ", apellido);
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let longitud = estudiante.length;
+let primera = nombre.charAt();
+let ultima = apellido.slice(-1);
+let espacio = estudiante.replace(/\s+/g, '');
+let contenido = estudiante.includes(nombre);
+
+
+console.log(estudiante);
+console.log(estudianteMayus);
+console.log(estudianteMinus);
+console.log(longitud);
+console.log(primera);
+console.log(ultima);
+console.log(espacio);
+console.log(contenido);
