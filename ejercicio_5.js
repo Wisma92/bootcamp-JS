a65ef9cb6d3d968f740db68e14436d4abaa6b2d8
Rafael Mendoza <112434814+Wisma92@users.noreply.github.com>
2023-06-21 19:52:13 -0500

    actividad 5

---
+++ ejercicio_5.js
@@ -0,0 +1,12 @@
+// Ejercicio 5
+
+let alturaCM = 175;
+let alturaM = 1.75;
+let peso = 64.5;
+let alturaRe = alturaM.toFixed();
+let pesoRE = parseInt(peso);
+let pregunta = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(alturaRe);
+console.log(pesoRE);
+console.log(pregunta);
