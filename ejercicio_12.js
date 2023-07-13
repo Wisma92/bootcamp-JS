2ac436c3c628c2ca7a0d0eafd0ff8e54b579feba
Rafael Mendoza <112434814+Wisma92@users.noreply.github.com>
2023-07-12 20:47:54 -0500

    actividad 12

---
+++ ejercicio_12.js
@@ -0,0 +1,16 @@
+function fib(num){
+    let a = 1;
+    let b = 1;
+    let lista = [1, 1];
+
+    for (let i = 3; i <= num; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+        lista.push(c)
+    }
+    return lista;
+    console.log(lista);
+}
+
+console.log(fib(6))
